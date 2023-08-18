b38f7186ec8bd2b7c593e8a8294351b3755e9c51
athuljain <athuljainkj2@gmail.com>
2023-08-18 10:04:33 +0530

    Final commit

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,29 @@
+import "./DigitalClock.css"
+const { useState, useEffect } = require("react")
+
+
+const DigitalClock=()=>{
+
+    const [CurrDateTime,setCurrDateTime]= useState(new Date())
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setCurrDateTime(new Date())
+        },1000)
+        return ()=>clearInterval(interval)
+    })
+
+    
+    return(
+        <div>
+        <div className="digital-clock">
+      <h4 className="clock-heading">Digital Clock</h4>
+      <h1 className="clock-time">{CurrDateTime.toLocaleString()}</h1>
+    </div>
+    <div>
+        <h6>Athul Jain</h6>
+    </div>
+    </div>
+    )
+}
+export default DigitalClock
